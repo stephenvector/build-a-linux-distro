63f280f1abbece87ed7d21bfb167cd65e612b0d3
Stephen Vector <55390907+stephenvector@users.noreply.github.com>
2019-11-10 11:02:25 -0500

    Update build-a-linux-os.sh

--- build-a-linux-os.sh
+++ build-a-linux-os.sh
@@ -8,6 +8,6 @@ apt-get install wget build-essential bison flex -y
 wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.3.10.tar.xz
 wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.3.10.tar.sign
 gpg --verify linux-5.3.10.tar.sign linux-5.3.10.tar.xz
-tar --xz linux-5.3.10.tar.xz
+tar -xz linux-5.3.10.tar.xz
 cd linux-5.3.10
 make defconfig
