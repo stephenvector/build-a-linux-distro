3dbe482b11e589e66a79da640990ff8e48bd38a4
Stephen Vector <stephenvector@pm.me>
2019-11-10 12:00:13 -0500

    Fixing gpg issue, making apt-get output quiet.

--- build-a-linux-os.sh
+++ build-a-linux-os.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get install wget build-essential bison flex xz gpg2 -y
+sudo apt-get update -q
+sudo apt-get upgrade -y -q
+sudo apt-get install wget build-essential bison flex xz gnupg2 -y -q
 
 # Download & verify kernel source
 wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.3.10.tar.xz
 wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.3.10.tar.sign
-gpg --locate-keys torvalds@kernel.org gregkh@kernel.org
-gpg --verify linux-5.3.10.tar.sign
+gpg2 --locate-keys torvalds@kernel.org gregkh@kernel.org
+gpg2 --verify linux-5.3.10.tar.sign
 unxz linux-5.3.10.tar.xz
-gpg --verify linux-5.3.10.tar.sign linux-5.3.10.tar
+gpg2 --verify linux-5.3.10.tar.sign linux-5.3.10.tar
 tar -xvf linux-5.3.10.tar
 cd linux-5.3.10
 make ARCH=x86_64 defconfig
