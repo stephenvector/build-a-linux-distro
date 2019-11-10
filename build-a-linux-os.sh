4efa19ed9318cddefd663bee16c4f883d54989bf
Stephen Vector <stephenvector@pm.me>
2019-11-10 11:46:24 -0500

    Fixing kernel build.

--- build-a-linux-os.sh
+++ build-a-linux-os.sh
@@ -2,14 +2,16 @@
 
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install wget build-essential bison flex xz -y
+sudo apt-get install wget build-essential bison flex xz gpg2 -y
 
 # Download & verify kernel source
 wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.3.10.tar.xz
 wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.3.10.tar.sign
-gpg2 --locate-keys torvalds@kernel.org gregkh@kernel.org
-gpg2 --verify linux-5.3.10.tar.sign
+gpg --locate-keys torvalds@kernel.org gregkh@kernel.org
+gpg --verify linux-5.3.10.tar.sign
 unxz linux-5.3.10.tar.xz
-gpg2 --verify linux-5.3.10.tar.sign linux-5.3.10.tar
+gpg --verify linux-5.3.10.tar.sign linux-5.3.10.tar
+tar -xvf linux-5.3.10.tar
 cd linux-5.3.10
 make ARCH=x86_64 defconfig
+make
