26487bf63ac3554a2eb4f29cb74cff3cf88d025d
Stephen Vector <stephenvector@users.noreply.github.com>
2020-04-26 17:03:01 -0400

    Hmmmmmm

--- build-a-linux-os.sh
+++ build-a-linux-os.sh
@@ -124,7 +124,8 @@ function build_a_linux_os {
   sudo parted -s $first_unused_loop_device mkpart primary fat32 1MiB 261MiB 
   sudo parted -s $first_unused_loop_device set 1 esp on
   sudo parted -s $first_unused_loop_device mkpart primary ext4 261MiB 100%
-  
+  sudo mkdir /mnt/efi
+  sudo mount $first_unused_loop_device /mnt/efi
   sudo grub-install --target=x86_64-efi --efi-directory=$first_unused_loop_device --bootloader-id=GRUB
   
   sudo lsblk -a
