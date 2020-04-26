4a701a8fa0e56f36e6859f2b64456b0b82fec256
Stephen Vector <stephenvector@users.noreply.github.com>
2020-04-26 14:48:27 -0400

    Hmmmmmmmmm

--- build-a-linux-os.sh
+++ build-a-linux-os.sh
@@ -114,16 +114,16 @@ function build_a_linux_os {
   #add_syslinux
   #add_grub
   dd if=/dev/zero of=os.img bs=1M count=512
-  sudo losetup -f > first_unused_loop_device    
+  first_unused_loop_device=$(sudo losetup -f)
   echo $first_unused_loop_device
   
 
-  #sudo losetup -P $first_unused_loop_device os.img
-  #mkfs.ext4 $first_unused_loop_device
-  #sudo parted -s $first_unused_loop_device mklabel gpt
-  #sudo parted -s $first_unused_loop_device mkpart primary fat32 1MiB 261MiB 
-  #   sudo parted -s $first_unused_loop_device set 1 boot on
-  #sudo parted -s $first_unused_loop_device mkpart primary ext4 261MiB 100%
+  sudo losetup -P $first_unused_loop_device os.img
+  mkfs.ext4 $first_unused_loop_device
+  sudo parted -s $first_unused_loop_device mklabel gpt
+  sudo parted -s $first_unused_loop_device mkpart primary fat32 1MiB 261MiB 
+  sudo parted -s $first_unused_loop_device set 1 boot on
+  sudo parted -s $first_unused_loop_device mkpart primary ext4 261MiB 100%
 }     
 
 build_a_linux_os
